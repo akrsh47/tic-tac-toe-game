50b506aa3b94a81b8d62287261382bcba5b248ba
Akarsh Balachandran <akarshbalu1@gmail.com>
2025-07-26 18:11:53 +0530

    basic setup,click button to show X

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [ticButtons, setTicButtons] = useState(Array(9).fill(""));
+
+  const winningSet = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const [writtenMarks, setWrittenMarks] = useState([]);
+
+  function handleButtonMark(indx) {
+    setWrittenMarks((prevItem) => [...prevItem, indx]);
+
+    setTicButtons((prevItem) => {
+      const updated = [...prevItem];
+      updated[indx] = "X";
+
+      return updated;
+    });
+  }
+  const renderTicButtons = ticButtons.map((button, index) => {
+    return (
+      <button
+        key={index}
+        className="ticButton"
+        onClick={() => handleButtonMark(index)}
+      >
+        {button}
+      </button>
+    );
+  });
+
+  console.log(writtenMarks);
+
+  return (
+    <>
+      <h1>XO</h1>
+      <section>
+        <h3>
+          <span>X</span> Player 1
+        </h3>
+        <h3>
+          <span>O</span> Player 2
+        </h3>
+      </section>
+
+      <section>Game Status: Playing...</section>
+      <br></br>
+      <section className="ticSec">{renderTicButtons}</section>
+    </>
+  );
+}
+
+export default App;
