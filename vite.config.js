c9c6ee01e1ddef73be0698372d170dbe33b1560e
Akarsh Balachandran <akarshbalu1@gmail.com>
2025-08-11 17:22:11 +0530

    added progressive web app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',          // auto update the SW when new build available
+      
+      manifest: {
+        name: 'X O.Tic,Tac,Win.',
+        short_name: 'X O',
+        description: 'A fun tic tac toe game',
+        theme_color: '#df20a3',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          { src: '/icon192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon512x512.png', sizes: '512x512', type: 'image/png' },
+          
+        ]
+      }
+    })
+  ]
 })
